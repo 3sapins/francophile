8cb1e7a9b2e62bad17c9587f59e2f666eecbb84c
3sapins <antoine.pochon@edu-vd.ch>
2026-02-04 20:07:46 +0100

    Add files via upload

--- src/config/config.php
+++ src/config/config.php
@@ -73,8 +73,9 @@ define('PERSONNES', [
 ]);
 define('PERSONNES_IMPERATIF', ['tu', 'nous', 'vous']);
 
-if (DEBUG_MODE) { error_reporting(E_ALL); ini_set('display_errors', 1); }
-else { error_reporting(0); ini_set('display_errors', 0); }
+// DEBUG TEMPORAIRE - à désactiver après diagnostic
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 date_default_timezone_set('Europe/Zurich');
 
